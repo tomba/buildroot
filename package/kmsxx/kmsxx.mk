08feee081c9090d31b32243033b5663209eb769b
Tomi Valkeinen <tomi.valkeinen@iki.fi>
2021-08-06 11:50:56 +0300

    Revert "HACK: kmsxx hacks to get pybind"
    This reverts commit 6f0c0ac326e5250f9ac54d1efab19f68846217fc.

--- package/kmsxx/kmsxx.mk
+++ package/kmsxx/kmsxx.mk
@@ -4,20 +4,18 @@
 #
 ################################################################################
 
-KMSXX_VERSION = e451c2d
-KMSXX_SITE = https://github.com/tomba/kmsxx.git
-KMSXX_SITE_METHOD = git
+KMSXX_VERSION = 548905611c130ea9d31929a0caf7a198d8bac462
+KMSXX_SITE = $(call github,tomba,kmsxx,$(KMSXX_VERSION))
 KMSXX_LICENSE = MPL-2.0
 KMSXX_LICENSE_FILES = LICENSE
 KMSXX_INSTALL_STAGING = YES
-KMSXX_GIT_SUBMODULES = YES
 KMSXX_DEPENDENCIES = fmt libdrm host-pkgconf
 KMSXX_CONF_OPTS = \
 	-Dkmscube=false \
-	-Dpykms=enabled \
-	-Dpyv4l2=enabled \
+	-Dpykms=disabled \
 	-Domap=disabled \
-	-Dlibutils=true -Dutils=true
+	-Duse-system-fmt=true \
+	-Duse-system-pybind11=true
 
 ifeq ($(BR2_STATIC_LIBS),y)
 KMSXX_CONF_OPTS += -Dstatic-libc=true
@@ -27,6 +25,25 @@ endif
 
 ifeq ($(BR2_TOOLCHAIN_HAS_GCC_BUG_85180),y)
 KMSXX_CXXFLAGS += $(TARGET_CXXFLAGS) -O0
+endif
+
+ifeq ($(BR2_PACKAGE_KMSXX_INSTALL_TESTS),y)
+KMSXX_CONF_OPTS += -Dutils=true
+# extra handling for some utils not installed by default
+KMSXX_EXTRA_UTILS = kmsview kmscapture omap-wbcap omap-wbm2m
+ifeq ($(BR2_PACKAGE_LIBEVDEV),y)
+KMSXX_DEPENDENCIES += libevdev
+KMSXX_EXTRA_UTILS += kmstouch
+endif
+define KMSXX_INSTALL_EXTRA_UTILS
+	$(foreach t,$(KMSXX_EXTRA_UTILS),\
+		$(INSTALL) -D -m 0755 $(@D)/build/utils/$(t) \
+			$(TARGET_DIR)/usr/bin/$(t)
+	)
+endef
+KMSXX_POST_INSTALL_TARGET_HOOKS += KMSXX_INSTALL_EXTRA_UTILS
+else
+KMSXX_CONF_OPTS += -Dutils=false
 endif
 
 $(eval $(meson-package))
